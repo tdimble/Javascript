1bd510a73fb467e4976afba224693ada1fa1483f
tdimble <tushar.dimble@exellarate.com>
2021-10-07 13:18:22 +0530

    Added Spread and Rest operator example.

---
+++ spread-rest-operator/1.js
@@ -0,0 +1,3 @@
+const oldArray = [1,2,3,4];
+const newArray = [...oldArray,5,6];
+console.log(newArray);
---
+++ spread-rest-operator/2.js
@@ -0,0 +1,7 @@
+const filter = (...args) => {
+
+   return args.filter(el => el === 1);
+
+}
+
+console.log(filter(1,2,3,4));
